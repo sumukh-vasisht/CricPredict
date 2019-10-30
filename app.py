a9521ec95f41806e39e1b180cb183aa6260f40f5
sumukh-vasisht <sumukhmys1999@gmail.com>
2019-10-30 15:32:03 +0530

    New html page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -7,9 +8,16 @@ def index():
     if request.method=="POST":
         team1 = request.form['team1']
         team2 = request.form['team2']
-        country = request.form['country']
-        return team1+team2+country
+        # country = request.form['country']
+        global mes
+        mes=team1+"."+team2
+        message=team1+"."+team2
+        return redirect(url_for('predicc',t1=team1,t2=team2))
     return render_template("index.html")
+
+@app.route('/predicc', methods=['GET','POST'])
+def predicc():
+    return render_template("page2.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
