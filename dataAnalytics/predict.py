295b6d2b3d105e5c4af0fc62b04fcfb532539f6b
sumukh-vasisht <sumukhmys1999@gmail.com>
2019-11-02 12:39:01 +0530

    Almost Done

--- dataAnalytics/predict.py
+++ dataAnalytics/predict.py
@@ -24,13 +24,15 @@ teams=[]
 
 team1=input("Enter Team 1 : ")
 team2=input("Enter Team 2 : ")
-teams.append(team1)
-teams.append(team2)
+tteam1=team1
+tteam2=team2
+teams.append(tteam1)
+teams.append(tteam2)
 # originalTeam1=team1
 # originalTeam2=team2
 teams.sort()
-team1=teams[0]
-team2=teams[1]
+tteam1=teams[0]
+tteam2=teams[1]
 # print("Team 1 : ",team1)
 # print("Team 2 : ",team2)
 venue=input("Enter Venue : ")
@@ -59,8 +61,8 @@ arr=np.array([[venueInt,tWinner,tDecision,int(0),int(0)]])
 # arr.reshape(1,-1)
 # print(arr.shape)
 
-t1=team1[:3]
-t2=team2[:3]
+t1=tteam1[:3]
+t2=tteam2[:3]
 fileName="./Matches/"+t1+t2+".txt"
 
 f=open(fileName,"r")
@@ -74,21 +76,34 @@ arr6=[float(s) for s in f.readline().split()]
 for i in range(len(arr6)):
     arr6[i]=round(arr6[i],2)
 
-print("arr1 : ",arr1)
-print("arr2 : ",arr2)
-print("arr3 : ",arr3)
-print("arr4 : ",arr4)
-print("arr5 : ",arr5)
-print("arr6 : ",arr6)
+# print("arr1 : ",arr1)
+# print("arr2 : ",arr2)
+# print("arr3 : ",arr3)
+# print("arr4 : ",arr4)
+# print("arr5 : ",arr5)
+# print("arr6 : ",arr6)
 
 features=np.array([arr1,arr3,arr4,arr5,arr6])
 features=features.transpose()
 arr2t=np.array(arr2)
 arr2t=arr2t.transpose()
-classLabelX=arr2t
+classLabelX=np.array(arr2t)
+print("--------------------------")
+print("--------------------------")
+print(classLabelX)
+print("--------------------------")
+print("--------------------------")
 
-clf=SVC(gamma='auto')
-clf.fit(features,classLabelX)
+# clf=SVC(gamma='auto')
+# clf.fit(features,classLabelX)
+
+try:
+    clf=SVC(gamma='auto')
+    clf.fit(features,classLabelX)
+except:
+    classLabelX[0]=-1
+    clf=SVC(gamma='auto')
+    clf.fit(features,classLabelX)
 
 clfDecisionTree=tree.DecisionTreeClassifier(max_depth=5)
 clfDecisionTree.fit(features,classLabelX)
@@ -162,7 +177,14 @@ maxValue = max(data,key=data.get)
 # print(maxValue)
 # print("------------------------")
 
-finalPred=clfAdaBoost.predict(arr)
+if maxValue=='ab':
+    finalPred=clfAdaBoost.predict(arr)
+elif maxValue=='dt':
+    finalPred=clfDecisionTree.predict(arr)
+elif maxValue=='rf':
+    finalPred=clfRandomForest.predict(arr)
+else:
+    finalPred=clfGaussian.predict(arr)
 # print(finalPred[0])
 winner=""
 if finalPred[0]==1:
